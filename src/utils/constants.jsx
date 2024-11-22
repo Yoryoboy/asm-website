54bb42dc0cdb852bfa14d7e5fb87bb50edd62925
Yoryoboy <93jads@gmail.com>
2024-11-22 10:52:54 -0300

    Refactor FeatureCard styles and apply consistent icon styling in constants for better layout and visuals

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -20,39 +20,41 @@ export const COMPANY_INFO = {
   EMAIL: "info@civilgroup.com",
 };
 
+const iconStyle = { strokeWidth: 2, width: "45px" };
+
 export const ABOUT_COMPANY = [
   {
-    icon: <HiOutlineBuildingOffice />,
+    icon: <HiOutlineBuildingOffice style={iconStyle} />,
     title: "Engineering Excellence",
     subtitle:
       "Structural and Civil Engineering services for private clients and public agencies.",
   },
   {
-    icon: <GrUserWorker />,
+    icon: <GrUserWorker style={iconStyle} />,
     title: "Calculated Inspections",
     subtitle:
       "We are as comfortable running job-site inspections, as punching out stress calculations.",
   },
   {
-    icon: <HiOutlineWrenchScrewdriver />,
+    icon: <HiOutlineWrenchScrewdriver style={iconStyle} />,
     title: "Design Expertise",
     subtitle:
       "The local landscape bears all our design expertise: residential, commercial, institutional, and medical structures, including mid and high-rise structures.",
   },
   {
-    icon: <HiOutlineUserGroup />,
+    icon: <HiOutlineUserGroup style={iconStyle} />,
     title: "Trusted Quality",
     subtitle:
       "ASM thrives on our commitment to quality, and reliable proposals to submit to owners, architects, construction companies, etc.",
   },
   {
-    icon: <GrMoney />,
+    icon: <GrMoney style={iconStyle} />,
     title: "Value Engineering",
     subtitle:
       "Understanding that our client's goal may be to minimize costs, ASM delivers quality projects to our clientele to best meet their cost objectives.",
   },
   {
-    icon: <HiOutlineShieldCheck />,
+    icon: <HiOutlineShieldCheck style={iconStyle} />,
     title: "Detailed Precision",
     subtitle:
       "We never sacrifice careful attention to detail in our plans and specifications.",
