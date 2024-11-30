f55ffba47b3cd51a994d1f41aba2ddcd5b811f91
Yoryoboy <93jads@gmail.com>
2024-11-30 11:04:56 -0300

    Refactor ContactUs styles for better layout consistency; add class to main for improved styling and responsiveness

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -2,9 +2,11 @@ import PageHeader from "../../components/PageHeader";
 import ContactForm from "./ContactForm";
 import img from "../../assets/images/contact-header.webp";
 
+import styles from "./ContactUs.module.css";
+
 function ContactUs() {
   return (
-    <main>
+    <main className={styles.contactUs}>
       <PageHeader title="contact us" img={img} />
       <ContactForm />
     </main>
