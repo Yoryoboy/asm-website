b4fe18131525b6a1829061c0c436e891f269a286
Yoryoboy <93jads@gmail.com>
2024-12-01 10:58:22 -0300

    Enhance ContactForm with loading state and submit functionality; improve form element handling for better user experience

--- src/pages/ContactUs/ContactForm.jsx
+++ src/pages/ContactUs/ContactForm.jsx
@@ -1,14 +1,58 @@
+import { useState } from "react";
+import { Spin } from "antd";
 import styles from "./ContactForm.module.css";
 
 function ContactForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  function sendForm(form, formElement) {
+    fetch("https://formsubmit.co/ajax/93jads@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setIsLoading(false);
+        formElement.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    const formElement = event.currentTarget;
+    const { elements } = formElement;
+
+    const nameInput = elements.namedItem("name");
+    const emailInput = elements.namedItem("email");
+    const phoneInput = elements.namedItem("phone");
+    const messageInput = elements.namedItem("message");
+
+    const form = {
+      name: nameInput.value,
+      email: emailInput.value,
+      phone: phoneInput.value,
+      message: messageInput.value,
+    };
+    sendForm(form, formElement);
+  }
+
   return (
-    <form className={styles.contactForm}>
+    <form className={styles.contactForm} onSubmit={handleSubmit}>
       <h1>Get in Touch</h1>
-      <input type="text" placeholder="Name*" required />
-      <input type="email" placeholder="Email*" required />
-      <input type="tel" placeholder="Phone" />
-      <textarea placeholder="Message"></textarea>
-      <button>SEND MESSAGE</button>
+      <input type="text" placeholder="Name*" name="name" required />
+      <input type="email" placeholder="Email*" name="email" required />
+      <input type="tel" placeholder="Phone" name="phone" />
+      <textarea placeholder="Message" name="message" required></textarea>
+      <button>{isLoading ? <Spin /> : "SEND MESSAGE"}</button>
     </form>
   );
 }
