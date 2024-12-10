bd91b425f456515434b9c8e450fb31fa59f5e95e
Yoryoboy <93jads@gmail.com>
2024-12-09 21:27:06 -0300

    Added Carousel in HomePage

--- src/pages/Home/WelcomeSection.jsx
+++ src/pages/Home/WelcomeSection.jsx
@@ -1,11 +1,46 @@
 import { Link } from "react-router-dom";
+import img1 from "../../assets/images/1713671901756.png";
+import img2 from "../../assets/images/Feature-7-Civil-Engineering-Advancements.jpg";
+import img3 from "../../assets/images/welcome-image.webp";
 import styles from "./WelcomeSection.module.css";
+import { Carousel } from "antd";
 
 function WelcomeSection() {
   return (
     <section className={styles.welcomeSection}>
+      <Carousel
+        autoplay
+        dots={false}
+        fade={true}
+        autoplaySpeed={4000}
+        speed={1500}
+      >
+        <div className={styles.imageWrapper}>
+          <img
+            className={styles.welcomeSectionImage}
+            src={img1}
+            alt="welcome-image"
+          />
+        </div>
+        <div className={styles.imageWrapper}>
+          <img
+            className={styles.welcomeSectionImage}
+            src={img2}
+            alt="welcome-image"
+          />
+        </div>
+        <div className={styles.imageWrapper}>
+          <img
+            className={styles.welcomeSectionImage}
+            src={img3}
+            alt="welcome-image"
+          />
+        </div>
+      </Carousel>
       <div className={styles.welcomeSectionContent}>
-        <h1>Welcome to ASM Consulting Engineers</h1>
+        <h1>
+          Welcome to <br /> ASM Consulting Engineers
+        </h1>
         <h2>
           Over 25 years of expertise in delivering reliable structural solutions
           with innovative civil engineering.
